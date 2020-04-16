1e9ece741bf7aab21daf8ea7566743b34b08696e
Eric Johnson <johnson.n.eric@gmail.com>
2020-04-16 21:54:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   final_array = []
   array = string.split
   array.each do |word|
-    final_arrayarray << word.reverse
+    final_array << word.reverse
   end
   final_array
   
